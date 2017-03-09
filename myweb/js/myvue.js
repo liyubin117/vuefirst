74edeccc8103e8a38789da1263c3572ecf17b65d
code159 <409901403@qq.com>
2017-03-09 22:49:01 +0800

    新增myweb

---
+++ myweb/js/myvue.js
@@ -0,0 +1,21 @@
+var user=new Vue({
+    el:'#Muser',
+    data:{
+        username:undefined,
+        password:undefined
+    },
+    methods:{
+        logon:function(){
+            if(this.username=='liyubin'&&this.password=='password'){
+                alert('登陆成功！')
+            }else if(this.username==null||this.password==null){
+                alert("请输入用户名或密码！")
+            }else{
+                alert("用户名或密码错误！")
+            }
+        },
+        register:function(){
+
+        }
+    }
+})
